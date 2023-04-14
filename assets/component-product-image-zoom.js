1c235aefe9f1d08c83ed111f0c3643f71d0e42c4
Peter Zelenicky <48129676+zelenicky@users.noreply.github.com>
2023-04-14 14:03:44 +0200

    Update component-product-image-zoom.js

--- assets/component-product-image-zoom.js
+++ assets/component-product-image-zoom.js
@@ -13,7 +13,7 @@ if (typeof ProductImageZoom !== 'function')
 				this.zoom = document.createElement('div');
 				this.zoom.id = 'zoom';
 				this.zoom.innerHTML = `
-					<img style="margin: 0 auto 0 auto; right: 0px;" />
+					<img class="zoom__exit" />
 					<span class="zoom__exit">${KROWN.settings.symbols.zoom_out}</span>
 					<span class="zoom__loader">${KROWN.settings.symbols.zoom_loader}</span>
 					<div class="zoom__overlay"></div>
